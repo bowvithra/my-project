c5e30af84a74ef1da79b69f012a0aec434d420e8
bowvithra <bowvithra@gmail.com>
2026-04-03 12:47:40 +0530

    Update email configuration with placeholder values
    Replaced hardcoded email credentials with placeholders.

--- backend/app.py
+++ backend/app.py
@@ -24,8 +24,8 @@ CORS(app)
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'abiliyak09@gmail.com'
-app.config['MAIL_PASSWORD'] = 'kdwpzwathkfepame'
+app.config['MAIL_USERNAME'] = 'your_mailid'
+app.config['MAIL_PASSWORD'] = 'your_password'
 
 mail = Mail(app)
 
